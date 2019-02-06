2d27da8cd7a02c9bc9cc03b701097042a8bdc671
dgrajewski <dawids115@gmail.com>
2019-02-06 21:09:58 +0100

    Made a relation between Action and Category;

--- app/src/main/java/pl/grajek/actions/model/entity/Action.kt
+++ app/src/main/java/pl/grajek/actions/model/entity/Action.kt
@@ -1,10 +1,20 @@
 package pl.grajek.actions.model.entity
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.ForeignKey.CASCADE
 import android.arch.persistence.room.PrimaryKey
 import java.util.*
 
-@Entity(tableName = "action_table")
+@Entity(
+    tableName = "action_table",
+    foreignKeys = [ForeignKey(
+        entity = Category::class,
+        parentColumns = ["id"],
+        childColumns = ["categoryId"],
+        onDelete = CASCADE
+    )]
+)
 data class Action(
     @PrimaryKey(autoGenerate = true) var id: Long?,
     var date: Date,
