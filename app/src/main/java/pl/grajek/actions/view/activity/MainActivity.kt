bda93994d915c849b23107dd7aba194e1e1a80aa
dgrajewski <dawids115@gmail.com>
2019-03-11 17:37:31 +0100

    Changed navigation drawer; Added natifications item;

--- app/src/main/java/pl/grajek/actions/view/activity/MainActivity.kt
+++ app/src/main/java/pl/grajek/actions/view/activity/MainActivity.kt
@@ -227,24 +227,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_notifications -> {
                 mainViewModel.activityToStart.value =
                     ActivityStartModel(NotificationActivity::class.java, Bundle())
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
             }
         }
 
